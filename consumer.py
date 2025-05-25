2aebe85c2cc9e1d19cad870f572b6cc43ce040e5
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2025-05-25 23:32:16 +0600

    addition

--- consumer.py
+++ consumer.py
@@ -40,8 +40,13 @@
 #     main()
 
 import pika
+import time
+import random
 def on_message_received(ch, method, properties, body):
-    print(f"message received {body}")
+    processing_time=random.randit(1,6)
+    print(f"received:{body},will take {processing_time} to process")
+    time.sleep(processing_time)
+    ch.basic_ack(delvery_tag = method.delivery_tag)
 connection_parameters = pika.ConnectionParameters('localhost')
 connection = pika.BlockingConnection(connection_parameters)
 channel = connection.channel()
