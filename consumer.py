883fcd63ea0963d0a2c20aea3b0ac79e82d42163
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2025-05-25 19:46:41 +0600

    new and updated version of practice

--- consumer.py
+++ consumer.py
@@ -1,18 +1,42 @@
-import pika
+# import pika
 
-# Подключаемся к RabbitMQ
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
+# # Подключаемся к RabbitMQ
+# connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+# channel = connection.channel()
 
-# Убеждаемся, что очередь 'hello' существует
-channel.queue_declare(queue='test_queue', durable=True)
+# # Убеждаемся, что очередь 'hello' существует
+# channel.queue_declare(queue='test_queue', durable=True)
 
-# Функция, которая срабатывает при получении сообщения
-def callback(ch, method, properties, body):
-    print(f" [x] Получено сообщение: {body.decode()}")
+# # Функция, которая срабатывает при получении сообщения
+# def callback(ch, method, properties, body):
+#     print(f" [x] Получено сообщение: {body.decode()}")
 
-# Подписываемся на очередь 'hello'
-channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
+# # Подписываемся на очередь 'hello'
+# channel.basic_consume(queue='hello', on_message_callback=callback, auto_ack=True)
 
-print(' [*] Ждём сообщений. Нажмите CTRL+C для выхода.')
-channel.start_consuming()
+# print(' [*] Ждём сообщений. Нажмите CTRL+C для выхода.')
+# channel.start_consuming()
+
+# from pika import ConnectionParameters, BlockingConnection
+# connection_params = ConnectionParameters(
+#     host = 'localhost',
+#     port = 5672,
+# )
+# def process_message(*args):
+#     for arg in args:
+#         print(arg, "\n\n")
+# def main():
+#     with BlockingConnection(connection_params) as conn:
+#         with conn.channel() as ch:
+#             ch.queue_declare(queue='messages')
+#             ch.basic_consume(
+#                 queue='messages',
+#                 on_message_callback = process_message,
+#             )
+#             print("жду сообщений")
+#             ch.start_consuming()
+
+# if __name__ == "__main__":
+#     main()
+
+import 
--- producer.py
+++ producer.py
@@ -1,16 +1,46 @@
+# import pika 
+
+# # Подключаемся к RabbitMQ (localhost = твой компьютер)
+# connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+# channel = connection.channel()
+
+# # Создаём очередь с именем 'hello'
+# channel.queue_declare(queue='hello')
+
+# # Отправляем сообщение
+# channel.basic_publish(exchange='',
+#                       routing_key='hello',
+#                       body='Привет от Producer!')
+# print(" [x] Отправлено: Привет от Producer!")
+
+# connection.close() 
+# from pika import ConnectionParameters, BlockingConnection
+# connection_params = ConnectionParameters(
+#     host = 'localhost',
+#     port = 5672,
+# )
+# def main():
+#     with BlockingConnection(connection_params) as conn:
+#         with conn.channel() as ch:
+#             ch.queue_declare(queue='messages')
+
+#             ch.basic_publish(
+#                 exchange="",
+#                 routing_key='messages',
+#                 body = 'Hello Rabbitmq.'
+#             )
+#             print('Message sent')
+
+# if __name__ == "__main__":
+#     main()
 import pika
-
-# Подключаемся к RabbitMQ (localhost = твой компьютер)
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+connection_parameters = pika.ConnectionParameters('localhost')
+connection = pika.BlockingConnection(connection_parameters)
 channel = connection.channel()
+channel.queue_declare(queue='letterbox')
 
-# Создаём очередь с именем 'hello'
-channel.queue_declare(queue='hello')
+message = 'Hello this is my second message or third message'
+channel.basic_publish (exchange = '', routing_key = 'letterbox', body = message)
+print(f'sent message: {message}')
+connection.close()
 
-# Отправляем сообщение
-channel.basic_publish(exchange='',
-                      routing_key='hello',
-                      body='Привет от Producer!')
-print(" [x] Отправлено: Привет от Producer!")
-
-connection.close() 
