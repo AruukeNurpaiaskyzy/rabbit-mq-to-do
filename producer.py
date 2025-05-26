f20691451aa261379ea361a0ee4966caed1d030e
AruukeNurpaiaskyzy <aruukelisa@gmail.com>
2025-05-26 18:10:41 +0600

    pubsub

--- producer.py
+++ producer.py
@@ -34,13 +34,14 @@
 # if __name__ == "__main__":
 #     main()
 import pika
+from pika.exchange_type import ExchangeType
 connection_parameters = pika.ConnectionParameters('localhost')
 connection = pika.BlockingConnection(connection_parameters)
 channel = connection.channel()
-channel.queue_declare(queue='letterbox')
+channel.exchange_declare(exchange='pubsub', exchange_type=ExchangeType.fanout)
 
-message = 'Hello this is my second message or third message'
-channel.basic_publish (exchange = '', routing_key = 'letterbox', body = message)
+message = 'Hello I want to broadcast this message'
+channel.basic_publish (exchange = 'pubsub', routing_key = '', body = message)
 print(f'sent message: {message}')
 connection.close()
 
